4c0a0666329c9eb3728270eb8a0d789c58209f92
RenatoUbuntu <renatoyferreira@gmail.com>
2023-05-30 18:39:36 -0300

    fixed: route timeline

--- src/pages/SigninPage/SigninPage.js
+++ src/pages/SigninPage/SigninPage.js
@@ -31,7 +31,7 @@ export default function SigninPage() {
     apiAuth.signin(form)
         .then(res => {
           setIsLoading(false)
-          navigate("/")
+          navigate("/timeline")
           
         })
         .catch(err => {
