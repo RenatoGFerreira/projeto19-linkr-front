0bfbc54ffd6ce815c444e4c615151b7f2ea66f96
Guilfornari <guilfornari@gmail.com>
2023-06-02 17:57:49 -0300

    implement get userlink by id

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import SigninPage from "./pages/SigninPage/SigninPage";
 import TimeLinePage from "./pages/TimeLinePage/TImeLine";
 import { useState } from "react";
 import { AuthContext } from "./contexts/AuthContext";
+import UserPage from "./pages/UserPage/UserPage";
 
 function App() {
   const [auth, setAuth] = useState({});
 
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{auth, setAuth}}>
+      <AuthContext.Provider value={{ auth, setAuth }}>
         <Routes>
           <Route path="/" element={<SigninPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/timeline" element={<TimeLinePage />} />
+          <Route path="/user/:id" element={<UserPage />} />
         </Routes>
       </AuthContext.Provider>
     </BrowserRouter>
