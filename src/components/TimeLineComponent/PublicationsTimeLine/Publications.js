3a14cd4489765d07cba49cf8d8d5afb9272b5a86
Guilfornari <guilfornari@gmail.com>
2023-06-05 12:13:38 -0300

    commit to update

--- src/components/TimeLineComponent/PublicationsTimeLine/Publications.js
+++ src/components/TimeLineComponent/PublicationsTimeLine/Publications.js
@@ -1,37 +1,31 @@
-import { useState } from "react";
 import { PublicationContainer, Image, Content, IconHeart, IconHeartfill, TextLike } from "./Style";
 import urlMetadata from "url-metadata";
 import { useEffect, useState } from "react";
 
-export default function Publication({ name, image, url, likes, description }) {
-    const [linkMetadata, setLinkMetadata] = useState(null);
-  
-    useEffect(() => {
-      fetchLinkMetadata();
-    }, [url]);
-  
-    async function fetchLinkMetadata() {
-      try {
-        const metadata = await urlMetadata(url);
-        console.log(metadata)
-        setLinkMetadata(metadata);
-      } catch (error) {
-        console.error("Ocorreu um erro ao buscar os metadados do link:", error);
-      }
-    }
+export default function Publication({ name, image, url, likes, description, user }) {
 
-export default function Publication({ user, description, url }) {
-  const [liked, setLiked] = useState(false)
-  function changeLike(){
-    setLiked(!liked)
+  const [linkMetadata, setLinkMetadata] = useState(null);
+
+  useEffect(() => {
+    fetchLinkMetadata();
+  }, [url]);
+
+  async function fetchLinkMetadata() {
+    try {
+      const metadata = await urlMetadata(url);
+      console.log(metadata)
+      setLinkMetadata(metadata);
+    } catch (error) {
+      console.error("Ocorreu um erro ao buscar os metadados do link:", error);
+    }
   }
-  
+
   return (
     <PublicationContainer>
       <Image>
-        <img src={image} alt="description"/>
-        <IconHeart/>
-        <IconHeartfill/>
+        <img src={image} alt="description" />
+        <IconHeart />
+        <IconHeartfill />
         <TextLike>13 likes</TextLike>
       </Image>
       <Content>
